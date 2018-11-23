b38f49fe2e2dd63ef34eaf578013c9ebc4566e7c
Scott Langridge <sml540@york.ac.uk>
2018-11-23 16:34:05 +0000

    Moved SeprGame.java into display package

--- SeprGame/desktop/src/com/mygdx/seprgame/desktop/DesktopLauncher.java
+++ SeprGame/desktop/src/com/mygdx/seprgame/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.mygdx.seprgame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.seprgame.SeprGame;
+import display.SeprGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
