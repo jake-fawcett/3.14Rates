1fee03bd169a081d92f05d68ba8405cbd549fea7
Scott Langridge <sml540@york.ac.uk>
2018-11-23 18:04:23 +0000

    Completed RoomUpgrade

--- SeprGame/core/src/combat/items/RoomUpgrade.java
+++ SeprGame/core/src/combat/items/RoomUpgrade.java
@@ -1,5 +1,25 @@
 package combat.items;
 
 public class RoomUpgrade {
+    private String name;
+    private int cost;
+    private double multiplier;
 
+    public RoomUpgrade(String name, int cost, double multiplier) {
+        this.name = name;
+        this.cost = cost;
+        this.multiplier = multiplier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public double getMultiplier() {
+        return multiplier;
+    }
 }
