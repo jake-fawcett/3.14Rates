1fb68cc6eeb2c671fb347a64cb96c8e44172a964
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-11-25 12:40:40 +0000

    Created body of ship

--- SeprGame/core/src/combat/ship/Ship.java
+++ SeprGame/core/src/combat/ship/Ship.java
@@ -1,5 +1,41 @@
 package combat.ship;
 
+import combat.items.Weapon;
+
+import java.util.List;
+
 public class Ship {
-    //TODO Scott working here - Adding body
+    private int crew;
+    private List<Room> rooms;
+    private List<Weapon> weapons;
+    private int baseHullHP;
+    private int hullHP;
+
+    public Ship(int crew, List<Room> rooms, List<Weapon> weapons, int baseHullHP, int hullHP) {
+        this.crew = crew;
+        this.rooms = rooms;
+        this.weapons = weapons;
+        this.baseHullHP = baseHullHP;
+        this.hullHP = hullHP;
+    }
+
+    public int getCrew() {
+        return crew;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public List<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    public int getBaseHullHP() {
+        return baseHullHP;
+    }
+
+    public int getHullHP() {
+        return hullHP;
+    }
 }
