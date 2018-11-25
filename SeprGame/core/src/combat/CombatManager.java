7030d9159354307906b8056cc846d6086b818178
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-11-25 13:59:21 +0000

    Created attributes and constructor for CombatManager

--- SeprGame/core/src/combat/CombatManager.java
+++ SeprGame/core/src/combat/CombatManager.java
@@ -1,5 +1,18 @@
 package combat;
 
+import combat.actors.CombatEnemy;
+import combat.actors.CombatPlayer;
+
 public class CombatManager {
+    private CombatPlayer player;
+    private CombatEnemy enemy;
+    private Boolean isPlayersTurn;
+
+    public CombatManager(CombatPlayer player, CombatEnemy enemy, Boolean isPlayersTurn) {
+        this.player = player;
+        this.enemy = enemy;
+        this.isPlayersTurn = isPlayersTurn;
+    }
+
 
 }
