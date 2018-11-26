06dd2351b91bc1b65eaddd993bbddb92672a58bc
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-11-26 18:31:36 +0000

    Created test files (but not tests) for CombatEnemy and Player

---
+++ SeprGame/core/src/testing/combat/actors/CombatEnemyTest.java
@@ -0,0 +1,12 @@
+package testing.combat.actors;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CombatEnemyTest {
+
+    @Test
+    public void takeTurn() {
+    }
+}
---
+++ SeprGame/core/src/testing/combat/actors/CombatPlayerTest.java
@@ -0,0 +1,12 @@
+package testing.combat.actors;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CombatPlayerTest {
+
+    @Test
+    public void takeTurn() {
+    }
+}
