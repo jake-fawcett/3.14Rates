3ff4e014bcf76474793b3ae292457deb83dcc68f
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-11-27 14:41:31 +0000

    Created blank tests for methods in CombatManager

---
+++ SeprGame/core/src/combat/manager/CombatManagerTest.java
@@ -0,0 +1,20 @@
+package combat.manager;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CombatManagerTest {
+
+    @Test
+    public void enterCombat() {
+    }
+
+    @Test
+    public void applyTurn() {
+    }
+
+    @Test
+    public void endCombat() {
+    }
+}
