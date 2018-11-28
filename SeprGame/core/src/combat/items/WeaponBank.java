9803ea76df74de65d02c7e93801c4ed553285828
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-11-28 14:57:42 +0000

    Fixed formatting and deleted useless duplicates

--- SeprGame/core/src/combat/items/WeaponBank.java
+++ SeprGame/core/src/combat/items/WeaponBank.java
@@ -3,12 +3,6 @@ package combat.items;
 public enum WeaponBank {
     //TODO Create weapons
     A(new Weapon("Storm Bringer", 200, 50, 100, 0.2,
-            0.9)),
-    B(new Weapon("Storm Bringer", 200, 50, 100, 0.2,
-            0.9)),
-    C(new Weapon("Storm Bringer", 200, 50, 100, 0.2,
-            0.9)),
-    D(new Weapon("Storm Bringer", 200, 50, 100, 0.2,
             0.9));
 
     private Weapon weapon;
@@ -21,4 +15,4 @@ public enum WeaponBank {
     WeaponBank(Weapon weapon) {
         this.weapon = weapon;
     }
-    }
+}
