25db6c6a2ea993329aaf37eed669b825587aab6a
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-11-28 14:59:53 +0000

    Fixed something I broke earlier with an accidental merge.

--- SeprGame/core/src/combat/items/Weapon.java
+++ SeprGame/core/src/combat/items/Weapon.java
@@ -2,7 +2,7 @@ package combat.items;
 
 public class Weapon {
     private String name;
-    public int cost;
+    private int cost;
     private int baseDamage;
     private int baseCooldown;
     private double baseCritChance;
