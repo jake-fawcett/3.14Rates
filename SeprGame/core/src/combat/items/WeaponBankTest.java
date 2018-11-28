fec2657c2a26226a79031cee49cbd2192e6b05f6
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-11-28 15:27:21 +0000

    Created tests for weapon bank

---
+++ SeprGame/core/src/combat/items/WeaponBankTest.java
@@ -0,0 +1,32 @@
+package combat.items;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WeaponBankTest {
+
+    @Test
+    public void testNotEmpty(){
+        assertFalse("These tests will not work with an empty WeaponBank",
+                WeaponBank.values().length == 0);
+    }
+
+    @Test
+    public void getWeapon(){
+        Weapon myWeapon = WeaponBank.values()[0].getWeapon();
+        assertEquals("You should be able to get a weapon class from the WeaponBank", Weapon.class,
+                myWeapon.getClass());
+    }
+
+    @Test
+    public void testGetWeaponCreatesNewInstances() {
+    Weapon w1 = WeaponBank.values()[0].getWeapon();
+    Weapon w2 = WeaponBank.values()[0].getWeapon();
+    System.out.println(w1.getCurrentCooldown());
+    w1.fire();
+    assertNotEquals("Weapons created from the same id should still be independent.", w1.getCurrentCooldown(),
+            w2.getCurrentCooldown());
+    }
+}
