ef34e78cf9d337309726e988fc5368328b17ad46
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-11-28 14:49:07 +0000

    Finished of WeaponBank ready for values

--- SeprGame/core/src/combat/items/WeaponBank.java
+++ SeprGame/core/src/combat/items/WeaponBank.java
@@ -1,12 +1,19 @@
 package combat.items;
 
 public enum WeaponBank {
-    TEST_WEAPON(new Weapon("Storm Bringer", 200, 50, 100, 0.2,
+    //TODO Create weapons
+    A(new Weapon("Storm Bringer", 200, 50, 100, 0.2,
+            0.9)),
+    B(new Weapon("Storm Bringer", 200, 50, 100, 0.2,
+            0.9)),
+    C(new Weapon("Storm Bringer", 200, 50, 100, 0.2,
+            0.9)),
+    D(new Weapon("Storm Bringer", 200, 50, 100, 0.2,
             0.9));
 
     private Weapon weapon;
 
-    public Weapon getWeapon(){
+    public Weapon getWeapon() {
         return new Weapon(weapon.getName(), weapon.getCost(), weapon.getBaseDamage(), weapon.getBaseCooldown(),
                 weapon.getBaseCritChance(), weapon.getBaseChanceToHit());
     }
@@ -14,4 +21,4 @@ public enum WeaponBank {
     WeaponBank(Weapon weapon) {
         this.weapon = weapon;
     }
-}
+    }
