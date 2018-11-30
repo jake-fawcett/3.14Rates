40574b228569349996b7be28fc153259540cb0a1
Scott Langridge <sml540@york.ac.uk>
2018-11-30 14:41:12 +0000

    Added test for decrement cooldown to weapon

--- SeprGame/core/src/combat/items/WeaponTest.java
+++ SeprGame/core/src/combat/items/WeaponTest.java
@@ -25,4 +25,15 @@ public class WeaponTest {
         assertEquals("Cooldown should be reset to base after fire", tester.getCurrentCooldown(),
                 tester.getBaseCooldown());
     }
+
+    @Test
+    public void decrementCooldown() {
+        tester.decrementCooldown(50);
+        assertEquals("decrementCooldown should never lower ticks past 0", 0,
+                tester.getCurrentCooldown());
+        tester.fire();
+        tester.decrementCooldown(50);
+        assertEquals("decrementCooldown should lower the cooldown by the given number of ticks",
+                tester.getBaseCooldown() - 50, tester.getCurrentCooldown());
+    }
 }
