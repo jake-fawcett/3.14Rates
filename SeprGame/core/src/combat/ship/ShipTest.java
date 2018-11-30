4c8927959de4642df674bd015931b67583c8d5f9
Scott Langridge <sml540@york.ac.uk>
2018-11-30 16:18:38 +0000

    Wrote tests for ship

--- SeprGame/core/src/combat/ship/ShipTest.java
+++ SeprGame/core/src/combat/ship/ShipTest.java
@@ -16,10 +16,21 @@ public class ShipTest {
 
     @Test
     public void damage() {
-
+        tester.damage(50);
+        assertEquals("HP should be reduced by 50", tester.getBaseHullHP() - 50,
+                tester.getHullHP());
+        tester.damage(tester.getBaseHullHP());
+        assertEquals("HP should not be allowed to go below 0", 0, tester.getHullHP());
     }
 
     @Test
     public void repair() {
+        tester.damage(tester.getBaseHullHP() - 1);
+        assertEquals("HP should be 1 before these tests run", 1, tester.getHullHP());
+        tester.repair(50);
+        assertEquals("HP should be raised to 51", 51, tester.getHullHP());
+        tester.repair(tester.getBaseHullHP());
+        assertEquals("HP should not be allowed to go above max", tester.getBaseHullHP(),
+                tester.getHullHP());
     }
 }
