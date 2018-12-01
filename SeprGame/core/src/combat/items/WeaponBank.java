8142552f81c4d755d23bbd780782db2922f7fdcf
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-12-01 20:05:28 +0000

    Create WeaponBank

--- SeprGame/core/src/combat/items/WeaponBank.java
+++ SeprGame/core/src/combat/items/WeaponBank.java
@@ -2,17 +2,27 @@ package combat.items;
 
 public enum WeaponBank {
     //TODO Create weapons
-    A(new Weapon("Storm Bringer", 200, 50, 100, 0.2,
-            0.9));
+    A("Storm Bringer", 200, 50, 100, 0.2, 0.9);
 
-    private Weapon weapon;
+    private String name;
+    private int cost;
+    private int baseDamage;
+    private int baseCooldown;
+    private double baseCritChance;
+    private double baseChanceToHit;
+
+    WeaponBank(String name, int cost, int baseDamage, int baseCooldown, double baseCritChance, double baseChanceToHit) {
+        this.name = name;
+        this.cost = cost;
+        this.baseDamage = baseDamage;
+        this.baseCooldown = baseCooldown;
+        this.baseCritChance = baseCritChance;
+        this.baseChanceToHit = baseChanceToHit;
+    }
 
     public Weapon getWeapon() {
-        return new Weapon(weapon.getName(), weapon.getCost(), weapon.getBaseDamage(), weapon.getBaseCooldown(),
-                weapon.getBaseCritChance(), weapon.getBaseChanceToHit());
+        return new Weapon(name, cost, baseDamage, baseCooldown, baseCritChance, baseChanceToHit);
     }
 
-    WeaponBank(Weapon weapon) {
-        this.weapon = weapon;
-    }
+
 }
