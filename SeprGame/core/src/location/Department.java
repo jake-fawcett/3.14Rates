1f7b54c970fef814f582e21cb0eb70b0a7588cd4
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-12-11 17:14:03 +0000

    Created outline for department

---
+++ SeprGame/core/src/location/Department.java
@@ -0,0 +1,45 @@
+package location;
+
+import combat.items.RoomUpgrade;
+import combat.items.Weapon;
+import other.Resource;
+
+import java.util.List;
+import java.util.Map;
+
+public class Department {
+    enum resources {
+        Weapons, Upgrades
+    }
+
+    private List<Weapon> weaponStock;
+    private List<RoomUpgrade> upgradeStock;
+    private Map<Resource, Integer> resourceStock;
+
+    public Department(List<Weapon> weaponStock, List<RoomUpgrade> upgradeStock, Map<Resource, Integer> resourceStock) {
+        this.weaponStock = weaponStock;
+        this.upgradeStock = upgradeStock;
+        this.resourceStock = resourceStock;
+    }
+
+    public List<Weapon> getWeaponStock() {
+        return weaponStock;
+    }
+
+    public List<RoomUpgrade> getUpgradeStock() {
+        return upgradeStock;
+    }
+
+    public Map<Resource, Integer> getResourceStock() {
+        return resourceStock;
+    }
+
+    public void buyWeapon(Integer index) {
+    }
+
+    public void buyRoomUpgrade(Integer index){
+    }
+
+    public void buyResource(Resource resource, int quantity) {
+    }
+}
