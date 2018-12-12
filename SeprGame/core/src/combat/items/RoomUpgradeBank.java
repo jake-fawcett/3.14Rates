bc3db90813fab57093d0287a121cfb58d3986623
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-12-12 16:46:28 +0000

    Fixed todo

--- SeprGame/core/src/combat/items/RoomUpgradeBank.java
+++ SeprGame/core/src/combat/items/RoomUpgradeBank.java
@@ -3,7 +3,7 @@ package combat.items;
 import combat.ship.RoomFunction;
 
 public enum RoomUpgradeBank {
-    //TODO Create weapons
+    //TODO Create Upgrades
     A("Refined Gunpowder", 150, 1.25, RoomFunction.GUN_DECK);
 
     private String name;
