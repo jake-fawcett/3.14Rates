c52e2add08cea1a21b7c3ea7a2100a29067e6703
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-12-12 12:15:20 +0000

    Started creating tests for department

--- SeprGame/core/src/location/DepartmentTest.java
+++ SeprGame/core/src/location/DepartmentTest.java
@@ -8,13 +8,15 @@ import static org.junit.Assert.*;
 import static testing_tools.SampleObjects.*;
 
 public class DepartmentTest {
+    //FIXME Scott working here
+
     private Department tester;
 
     @Before
     public void setUp() {
-        tester = new Department(createSampleWeapons(), createSampleUpgrades(RoomFunction.CROWS_NEST, 2),
+        tester = new Department(createSampleWeapons(1), createSampleUpgradeStock(2),
                 createSampleResourceStock(1));
-    }
+}
 
     @Test
     public void buyWeapon() {
