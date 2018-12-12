5ff0aeff8ee8c57161afd9f4645d2cce7c11886f
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-12-12 16:56:26 +0000

    Created ShipBank

---
+++ SeprGame/core/src/combat/ship/ShipBank.java
@@ -0,0 +1,28 @@
+package combat.ship;
+
+import combat.items.Weapon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum ShipBank {
+    //TODO Create Ships
+    //TODO replace new lists below with actual pre-made lists of rooms and weapons.
+    STARTER_SHIP(20, new ArrayList<Room>(), new ArrayList<Weapon>(), 100);
+
+    private int crew;
+    private List<Room> rooms;
+    private List<Weapon> weapons;
+    private int baseHullHP;
+
+    ShipBank(int crew, List<Room> rooms, List<Weapon> weapons, int baseHullHP) {
+        this.crew = crew;
+        this.rooms = rooms;
+        this.weapons = weapons;
+        this.baseHullHP = baseHullHP;
+    }
+
+    public Ship getShip() {
+        return new Ship(crew, rooms, weapons, baseHullHP);
+    }
+}
---
+++ SeprGame/core/src/combat/ship/ShipBankTest.java
@@ -0,0 +1,23 @@
+package combat.ship;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class ShipBankTest {
+
+    @Before
+    public void testNotEmpty(){
+        assertNotEquals("These tests will not work with an empty ShipBank",
+                ShipBank.values().length == 0);
+    }
+
+    @Test
+    public void getShipGetsShip(){
+        Ship myShip = ShipBank.STARTER_SHIP.getShip();
+        assertEquals("You should be able to get a ship from the ShipBank", Ship.class,
+                myShip.getClass());
+    }
+}
