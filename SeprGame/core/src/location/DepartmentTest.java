55aa60b7793cd8f8191902e423b70f36124ce6da
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-12-12 13:26:15 +0000

    wrote tests for buying weapon

--- SeprGame/core/src/location/DepartmentTest.java
+++ SeprGame/core/src/location/DepartmentTest.java
@@ -1,8 +1,12 @@
 package location;
 
+import combat.items.Weapon;
 import combat.ship.RoomFunction;
+import combat.ship.Ship;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static testing_tools.SampleObjects.*;
@@ -11,15 +15,32 @@ public class DepartmentTest {
     //FIXME Scott working here
 
     private Department tester;
+    private Ship testShip;
+
 
     @Before
     public void setUp() {
         tester = new Department(createSampleWeapons(1), createSampleUpgradeStock(2),
                 createSampleResourceStock(1));
-}
+        testShip = createSampleShip(0);
+    }
 
     @Test
-    public void buyWeapon() {
+    public void buyWeaponMovesWeapon() {
+        Weapon buying = tester.getWeaponStock().get(0);
+        tester.buyWeapon(testShip, 0);
+        assertFalse("Weapon should be removed from stock", tester.getWeaponStock().contains(buying));
+        assertTrue("Weapon should be added to ship", testShip.getWeapons().contains(buying));
+    }
+
+    @Test
+    public void buyWeaponBuysCorrectWeapon() {
+        assertTrue("For this test to work weapon stock must be at least of length 5", 
+                tester.getWeaponStock().size() >= 5);
+        Weapon buying = tester.getWeaponStock().get(2);
+        tester.buyWeapon(testShip, 2);
+        assertFalse("Weapon should be removed from stock", tester.getWeaponStock().contains(buying));
+        assertTrue("Weapon should be added to ship", testShip.getWeapons().contains(buying));
     }
 
     @Test
