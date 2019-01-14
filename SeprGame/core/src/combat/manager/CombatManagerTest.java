1dfdefd3c2b954b69e712b25a3f4fcd417159d5f
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2019-01-14 21:11:18 +0000

    Created setup for tests and created rough combat loop

--- SeprGame/core/src/combat/manager/CombatManagerTest.java
+++ SeprGame/core/src/combat/manager/CombatManagerTest.java
@@ -1,25 +1,21 @@
 package combat.manager;
 
+import banks.ShipBank;
+import combat.actors.CombatEnemy;
+import combat.actors.CombatPlayer;
+import combat.ship.Ship;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class CombatManagerTest {
+    CombatManager tester;
 
-    @Test
-    @Ignore
-    public void enterCombat() {
-//    Todo implement -("Test not yet implemented");
+    @Before
+    public void setUp(){
+        System.out.println("here");
+        tester = new CombatManager(new CombatPlayer(ShipBank.STARTER_SHIP.getShip()),
+                new CombatEnemy(ShipBank.STARTER_SHIP.getShip()));
     }
 
-    @Test
-    @Ignore
-    public void applyTurn() {
-//   Todo implement - fail("Test not yet implemented");
-    }
-
-    @Test
-    @Ignore
-    public void endCombat() {
-//   Todo implement - fail("Test not yet implemented");
-    }
 }
