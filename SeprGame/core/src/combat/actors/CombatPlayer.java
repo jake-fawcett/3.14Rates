ae0227efec8245f27c62b85ed7a6ad2a46dfd192
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2019-01-19 14:27:45 +0000

    Created outline

--- SeprGame/core/src/combat/actors/CombatPlayer.java
+++ SeprGame/core/src/combat/actors/CombatPlayer.java
@@ -5,7 +5,10 @@ import combat.ship.Room;
 import combat.ship.Ship;
 import javafx.util.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static other.Constants.COOLDOWN_TICKS_PER_TURN;
 
 public class CombatPlayer extends CombatActor {
     public CombatPlayer(Ship ship) {
@@ -13,8 +16,24 @@ public class CombatPlayer extends CombatActor {
     }
 
     @Override
+    /**
+     * Takes a damage report telling you what has been attacked, decrements cooldowns of weapons, repairs the ship
+     * and fires on enemy by returning an attack report.
+     */
     public List<Pair<Room, Weapon>> takeTurn(List<Pair<Room, Integer>> damageReport, Ship enemy) {
-        return null;
+        List<Pair<Room, Weapon>> attackReport = new ArrayList<Pair<Room, Weapon>>();
+        for (Weapon weapon : getShip().getWeapons()) {
+            weapon.decrementCooldown(COOLDOWN_TICKS_PER_TURN);
+        }
+        getShip().combatRepair();
+
+
+
+        //GENERATE ATTACK REPORT HERE
+
+
+
+        return attackReport;
 //        TODO Write me
     }
 }
