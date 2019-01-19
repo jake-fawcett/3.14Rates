9f39ae7f01330d4b6bc7fa02d47e2da1ea8f3464
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2019-01-19 15:19:34 +0000

    Documentation on Items

--- SeprGame/core/src/combat/items/RoomUpgrade.java
+++ SeprGame/core/src/combat/items/RoomUpgrade.java
@@ -2,10 +2,27 @@ package combat.items;
 
 import combat.ship.RoomFunction;
 
+/**
+ * An upgrade that can be bought from departments and
+ * applied to a room. It increases the effectiveness of that room in combat.
+ */
 public class RoomUpgrade {
+    /**
+     * Name of the item displayed in shops etc.
+     */
     private String name;
+    /**
+     * Price of the item in shops.
+     */
     private int cost;
+    /**
+     * The multiplier it has on the room that it effects. E.g. 2.0 would double the effectiveness of the room it is
+     * applied to.
+     */
     private double multiplier;
+    /**
+     * The type of room that it effects. E.g. GUN_DECK would increase the damage fo each shot.
+     */
     private RoomFunction affectsRoom;
 
     public String getName() {
