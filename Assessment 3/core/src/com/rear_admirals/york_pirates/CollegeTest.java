03621b94bac9afd28a795f7cf665bbe83e619dd3
Scott Langridge <sml540@york.ac.uk>
2019-02-12 13:50:28 +0000

    Created tests for college

---
+++ Assessment 3/core/src/com/rear_admirals/york_pirates/CollegeTest.java
@@ -0,0 +1,31 @@
+package com.rear_admirals.york_pirates;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class CollegeTest {
+    private College tester;
+
+    @Before
+    public void setUp() throws Exception {
+        tester = College.Derwent;
+    }
+
+    @Test
+    public void addAlly() {
+        College adding = new College("Test");
+        tester.addAlly(adding);
+        assertTrue(tester.getAlly().contains(adding));
+    }
+
+    @Test
+    public void isBossDead() {
+        boolean before = tester.isBossDead();
+        tester.setBossDead(!before);
+        assertNotEquals(before, tester.isBossDead());
+    }
+}
