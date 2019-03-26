4be9019758b8bfbb77e76caaf3df4bff8fc64669
JakeF <jakefawcett2298@hotmail.co.uk>
2019-03-26 15:21:29 +0000

    Added Code to Create and Connect Monster Node and to Switch the Screen to monster screen

--- Assessment 4/core/src/main/java/uk/ac/york/sepr4/ahod2/node/MonsterNode.java
+++ Assessment 4/core/src/main/java/uk/ac/york/sepr4/ahod2/node/MonsterNode.java
@@ -1,4 +1,20 @@
 package uk.ac.york.sepr4.ahod2.node;
 
-public class MonsterNode {
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import uk.ac.york.sepr4.ahod2.GameInstance;
+import uk.ac.york.sepr4.ahod2.io.FileManager;
+import uk.ac.york.sepr4.ahod2.screen.MonsterScreen;
+
+public class MonsterNode extends Node {
+
+    public MonsterNode(Node node) {
+        super(node.getId(), node.getRow(), node.getCol());
+        setConnected(node.getConnected());
+        this.setTexture(new TextureRegionDrawable(new TextureRegion(FileManager.seamonsterNodeIcon)));
+    }
+
+    public void action(GameInstance gameInstance) {
+        gameInstance.fadeSwitchScreen(new MonsterScreen(gameInstance));
+    }
 }
